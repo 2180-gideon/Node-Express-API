d4117bb7b6df792406feef6657a9abf0f166402a
Gideon Choge <55392079+2180-gideon@users.noreply.github.com>
2022-07-20 17:19:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const res = require('express/lib/response');
+
+//Routes
+const usersRoutes = require('./routes/users.js');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('Hello from Home Page'));
+    
+app.listen(PORT, () =>  console.log(`Server running on port: http://localhost:${PORT}`));
